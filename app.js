86402a4832f830e1b84d5e8d9f4ca867a480cfa3
rbs517 <rebeccaskurnik@gmail.com>
2018-10-22 21:26:06 -0400

    added html

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-app.listen(process.env.PORT);
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
