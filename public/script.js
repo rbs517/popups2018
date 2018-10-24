3e85b7061c9e438f54617f839baada82fe4648dd
rbs517 <rebeccaskurnik@gmail.com>
2018-10-24 18:03:34 -0400

    update css

--- public/script.js
+++ public/script.js
@@ -31,6 +31,30 @@ socket.on('dimensions', function(data) {
   console.log('new data came to light...');
 });
 
+
+// ********************************************************** 
+// BOOTSTRAP 
+// * Start Bootstrap - New Age v5.0.0 (https://startbootstrap.com/template-overviews/new-age)
+// * Copyright 2013-2018 Start Bootstrap
+// * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-new-age/blob/master/LICENSE)
+ 
+// Scroll trigger
+!function(e){
+  "use strict";e('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(){
+    if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){
+      var a=e(this.hash);if((a=a.length?a:e("[name="+this.hash.slice(1)+"]")).length)return e("html, body").animate({
+        scrollTop:a.offset().top-48},1e3,"easeInOutExpo"),!1
+      }
+    }),
+  e(".js-scroll-trigger").click(function(){
+  
+  }),
+
+  a(),e(window).scroll(a);
+
+}(jQuery);
+
+
 // ********************************************************** 
 // STARTING JS FILE
 
