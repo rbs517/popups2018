4d278a40e8adbff01a40dc1bb22c1162d563e22d
rbs517 <rebeccaskurnik@gmail.com>
2018-10-23 20:21:08 -0400

    fixed socket.io issue. Changed app.listen to http.listen

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var app = express ();
-// var path = require('path');
+var path = require('path');
 var PORT = process.env.PORT || 5000;
 var Request = require('request');
-// var http = require('http').Server(app);
 var http = require('http').Server(app);
 
-app.use(express.static( __dirname + '/public' ));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 app.set('port', PORT);
@@ -21,10 +19,6 @@ app.get('/', function(req, res){
 // 	res.send('Ooops.. nothing here.');
 // });
 
-
-// var server = app.listen(PORT);
-
-// var server = http.createServer(app);
 console.log("App is served on localhost: " + PORT);
 
 var io = require('socket.io')(http);
