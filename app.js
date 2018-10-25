d0622f45391c25ec974f35e3ea2364ed6511efee
rbs517 <rebeccaskurnik@gmail.com>
2018-10-25 13:47:11 -0400

    cleaning up code

--- app.js
+++ app.js
@@ -27,17 +27,6 @@ app.get('/', function(req, res){
 //   res.render('data');
 // });
 
-var pixel = require("node-pixel");
-var five = require('johnny-five');
-var board = new five.Board();
-var strip = null;
-
-// // Post request
-// app.post('/', function(req, res){
-//   console.log('user enters..');
-//   res.render('index');
-// });
-
 console.log("App is served on localhost: " + PORT);
 
 // **********************************************************
@@ -51,14 +40,6 @@ io.on('connection', function(socket){
   userCount = userCount + 1;
   console.log('a user connected');
   console.log('number of connected users: ' + userCount);
-
-//example from docs
-  // socket.emit('news', { hello: 'world' }); //emitting data to html console
-  // socket.on('my other event', function (data) {
-  // console.log(data);
-  // });
-  //io.sockets.emit('userCount', userCount);
-
 
 // On disconnect to socket
   socket.on('disconnect', function(){
@@ -76,61 +57,49 @@ io.on('connection', function(socket){
     // io.sockets.emit('dimensions', {h: h});
   });
 
-  socket.on('mouse', mouseMsg);
-  board.on('ready', function() {
-    boardIsReady = true;
+  //LED STUFF
+  // socket.on('mouse', mouseMsg);
+  // board.on('ready', function() {
+  //   boardIsReady = true;
 
-    strip = new pixel.Strip({
-      board: this,
-      controller: "FIRMATA",
-      strips: [{
-        pin: 10,
-        length: 12
-      }, ],
-      gamma: 2.8,
-    });
+  //   strip = new pixel.Strip({
+  //     board: this,
+  //     controller: "FIRMATA",
+  //     strips: [{
+  //       pin: 10,
+  //       length: 12
+  //     }, ],
+  //     gamma: 2.8,
+  //   });
 
-    strip.on("ready", function() {
-      // Set the entire strip to pink.
-      strip.color('#903');
+  //   strip.on("ready", function() {
+  //     // Set the entire strip to pink.
+  //     strip.color('#903');
 
-      // Send instructions to NeoPixel.
-      strip.show();
-    });
+  //     // Send instructions to NeoPixel.
+  //     strip.show();
+  //   });
 
-    this.repl.inject({
-      strip: strip
-    });
+  //   this.repl.inject({
+  //     strip: strip
+  //   });
 
-  });
-
-  function mouseMsg(data) {
-    // socket.broadcast.emit('mouse', data); //
-    // io.socket.emit('mouse', data); // including client who sends the msg
-    // console.log(socket.id + ': ' + data);
-
-    if (boardIsReady) {
-      var led = new five.Led(5); // pin 13
-      led.brightness(data);
-      console.log(data);
-      led = new five.Led(6); // pin 13
-      led.brightness(0);
-    }  
-  }
-
-
-  // socket.on('newData', function(data) {
-  //     console.log('im the server and i see ur: ' + data);
-  //     // io.emit('message', msg);
   // });
 
-  // socket.on('newData', function(data) {
+  // function mouseMsg(data) {
+  //   // socket.broadcast.emit('mouse', data); //
+  //   // io.socket.emit('mouse', data); // including client who sends the msg
+  //   // console.log(socket.id + ': ' + data);
+
+  //   if (boardIsReady) {
+  //     var led = new five.Led(5); // pin 13
+  //     led.brightness(data);
   //     console.log(data);
-  // });
+  //     led = new five.Led(6); // pin 13
+  //     led.brightness(0);
+  //   }  
+  // }
 
-  // socket.on('newData', function(){
-  //   console.log("hey! I am the app and I saw new data");
-  // });
 
   function restart (){
     final = 0;
--- public/script.js
+++ public/script.js
@@ -10,11 +10,9 @@ var data;
 var isStop = false;
 
 
-
 //Disable longpress on mobile devices
 function longClickHandler(e) {
   e.preventDefault();
-  // $("body").append("<p>You longclicked. Nice!</p>");
 }
 
 $("div.circleContainer").longclick(250, longClickHandler);
@@ -30,24 +28,6 @@ $(function() {
     console.log(event.target.id);
     blowVal(); //only if you are pressing, will the mic be listening
     socket.emit('pressed', 'pressed');
-    
-    // socket.emit('mouse', data);
-
-    // socket.emit('newData', 
-    // {my: 'data'
-    //   // (touchValue) =>{
-    //   // console.log(touchValue);
-    //   // console.log(touchValue +4);
-    // });
-
-    // //example from docs
-    // socket.on('news', function(data) {
-    //   console.log(data);
-    //   socket.emit('my other event', {
-    //     my: 'data'
-    //   });
-    // });
-
   }
 });
 
@@ -60,125 +40,34 @@ $(document).on("vmouseup", function() {
 });
 
 
-
-
 function setup() {
-
   mic = new p5.AudioIn();
   mic.start();
-
-  // createCanvas(200, 200);
-  // background(200);
-  // // var button = createButton("Start Blowing");
-  // // button.style("background-color:red");
-  // socket = io.connect('https://fluto.ngrok.io');
-  // // socket.on('mouse', newDrawing);
 }
 
-// function newDrawing(data) {
-  // background(200);
-  // fill(127);
-  // stroke(0);
-  // ellipse(width / 2, data - 25, 50, 50);
-
-// }
-
 function blowVal() {
-
-  // var interval = setInterval(function() {
     vol = mic.getLevel();
     // h = map(vol, 0, 1, height, 0); //for ellipse
     h = map(vol, 0.3, 1, 130, 255); //for fan
     data = h;
-    // if (data )
-    //newDrawing(data);
-    // var myBtn = document.getElementById('circle1');
-    // myBtn.style.backgroundColor = "red";
-    // myBtn.disabled = true;
     socket.emit('mouse', data);
     console.log('Sending: ' + data);
-  // }, 10);
-
-
-
-  // setTimeout(function() {
-  //   // isStop = true;
-  //   myBtn.disabled = false;
-  //   clearInterval(interval);
-  // }, 5000);
-
-  // setTimeout(function() {
-  //   myBtn.style.backgroundColor = '#4CAF50';
-  //   data = 125;
-  //   socket.emit('mouse', data);
-  //   console.log('Sending: ' + data);
-  // }, 5100);
-
 }
-
-
-// var setBtn = document.getElementById('myBtn');
-// setBtn.addEventListener("click", function() {
-//   blowVal();
-// });
-
 
 function draw() {
 
-  // blowVal();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // ********************************************************** 
 // SOCKET COMMUNICATION ON CLIENT SIDE
 
 var socket = io();
-// Value of Pressed or not 0 = off, 1 = on
-// var touchValue = 0;
 
 socket.emit('user', 'new user is connected');
 socket.on('userCount', function(userCount) {
   // console.log('total number of users online is: ' + userCount);
 });
-
-// socket.on('dimensions', function(data){
-//   w = data.w;
-//   h = data.h;
-// });
-// //example from docs
-// socket.on('news', function(data) {
-//   console.log(data);
-//   socket.emit('my other event', {
-//     my: 'data'
-//   });
-// });
-
-// socket.on('receiveMessage', function(msg) {
-//   console.log(msg);
-//   console.log("hello");
-// });
-
-// socket.on('tapholdData', function(colorTap){
-//   console.log('Color: ' + color);
-// });
-
-// socket.on('dimensions', function(data) {
-//   console.log('new data came to light...');
-// });
 
 
 // ********************************************************** 
