8b95d6c86576b0f2077bd50f70076ba36ba4238a
rbs517 <rebeccaskurnik@gmail.com>
2018-10-27 22:54:10 -0400

    mehhhh

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ io.on('connection', function(socket){
   //trying this
   socket.on('pressed', function(){
     console.log('got a press');
+    socket.emit('server is confirming pressed');
     //socket.broadcast.emit('broadcasting');
     //socket.broadcast.emit('broadcast', 'hello friends!');
     // io.sockets.emit('dimensions', {h: h});
