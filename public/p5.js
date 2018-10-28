87038b8c59a732785b7d4503a633999d4877d0c1
rbs517 <rebeccaskurnik@gmail.com>
2018-10-28 04:59:32 -0400

    trying to combine p5 serialport and socket.io

--- public/p5.js
+++ public/p5.js
@@ -45,6 +45,21 @@ function portOpen() {
   console.log('the serial port opened.');
 }
 
+
+//Data smoothing functions
+function smoothReading(newReading) {
+  blowData.shift();
+  blowData.push(newReading);
+  var total = 0;
+  for (var i = 0; i < blowData.length; i++) {
+    total += blowData[i];
+  }
+  var avg = total / blowData.length;
+  return avg;
+  //maybe use math.floor and do more elegant control on the arduino side
+}
+
+
 function serialEvent() {
   var inString = serial.readStringUntil('\r\n');
   // console.log(haveibeenpressed);
