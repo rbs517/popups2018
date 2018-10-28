319e0ac23a63ae4bd72c1f1b0e18cfbe35aac61b
rbs517 <rebeccaskurnik@gmail.com>
2018-10-28 04:44:03 -0400

    trying to combine p5 serialport and socket.io

--- public/script.js
+++ public/script.js
@@ -26,6 +26,10 @@ function setup() {
 }
 
 function draw() {
+  vol = mic.getLevel();
+  micInput = map(vol, 0, 0.4, 1, 255); //inputVal is for arduino to control the fan
+  // tell the server that the button has been pressed
+  // socket.emit('testingMic', micInput);
   // inputVal = micInput;
 }
 
@@ -51,11 +55,6 @@ $(function() {
     colorNum = idString.slice(6); //slice the string so it only prints the circle number
     // console.log(colorSelection); //print button color number
     socket.emit('pressed', colorNum);
-
-    vol = mic.getLevel();
-    micInput = map(vol, 0, 0.4, 1, 255); //inputVal is for arduino to control the fan
-    // tell the server that the button has been pressed
-    // socket.emit('testingMic', micInput);
 
     // tell the server that the button has been pressed
   //   socket.emit('pressed', colorNum,function(data){
