91cd2787d93a41bb895d0cc2d3ed8d68ef8e03f7
rbs517 <rebeccaskurnik@gmail.com>
2018-11-01 15:24:28 -0400

    disabling the buttons that are in use by other users

--- public/script.js
+++ public/script.js
@@ -203,7 +203,7 @@ socket.on('userCount', function(userCount) {
 // if pressed
 socket.on('colorPressed', function(colorNum, idString){
   //disable button --change to grey
-  removeTap(idString);
+  $('#' + idString).removeClass('tap');
   $('#' + idString).addClass('turnGrey');
   console.log('colorNum: ' + colorNum + ' is taken!');
 });
