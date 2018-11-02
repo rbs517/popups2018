def3b99c0de96cca6562a2e3b6b2b713feba978e
rbs517 <rebeccaskurnik@gmail.com>
2018-11-02 13:35:49 -0400

    trying out different methods of disabling and enabling buttons

--- public/script.js
+++ public/script.js
@@ -205,10 +205,10 @@ socket.on('userCount', function(userCount) {
 socket.on('colorPressed', function(colorNum){
 console.log("Got colorPressed: " + colorNum);
   //disable button --change to grey
-  $('#' + 'circle' + colorNum).removeClass('tap');
-  $('#' + 'circle' + colorNum).addClass('turnGray');
-  $('#' + 'circle' + colorNum).unbind("vmousedown", function(){
 
+  $('#' + 'circle' + colorNum).unbind("vmousedown", function(){
+    $('#' + 'circle' + colorNum).removeClass('tap');
+    $('#' + 'circle' + colorNum).addClass('turnGray');
   });
   console.log('colorNum: ' + colorNum + ' is taken!');  
 });
