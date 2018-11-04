00cefaf06c42c570ace97d23eca910e2cc0c8a99
rbs517 <rebeccaskurnik@gmail.com>
2018-11-04 01:03:44 -0400

    updating

--- public/script.js
+++ public/script.js
@@ -133,20 +133,19 @@ socket.on('colorPressed', function(colorNum){
   // console.log("Got colorPressed: " + colorNum);
   //disable button --change to grey
   //$('#' + 'circle' + colorNum).addClass('turnGray');
-  $('#' + 'circle' + colorNum).unbind("vmousedown");
-  $('#' + 'circle' + colorNum).removeClass('tap');
+  $('#' + 'circle' + colorNum).unbind("vmousedown"); 
+  $('#' + 'circle' + colorNum).removeClass('tap'); //stop the white rim
 
   // set timeout after 8 seconds to release the button 
-  setTimeout(function() { turnGray(colorNum); }, 8000);
-
-  removeTap(colorNum);
+  // setTimeout(function() { turnGray(colorNum); }, 8000);
+  setTimeout(function() { removeTap(colorNum); }, 8000);
 
   console.log('colorNum: ' + colorNum + ' is taken!');  
 });
 
-function turnGray(colorNum){
-    $('#' + 'circle' + colorNum).addClass('turnGray');
-}
+// function turnGray(colorNum){
+//     $('#' + 'circle' + colorNum).addClass('turnGray');
+// }
 // socket.on('toClients', function(colorNum){
 //   //enable button --change to normal color state
 //   $('#' + 'circle' + colorNum).removeClass('turnGray');
