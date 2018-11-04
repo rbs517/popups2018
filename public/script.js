f66ad5444cf078c247621442843332a9f6cb5ee3
rbs517 <rebeccaskurnik@gmail.com>
2018-11-04 01:43:41 -0400

    updating

--- public/script.js
+++ public/script.js
@@ -132,19 +132,14 @@ socket.on('toColorPresser', function(colorNum){
 socket.on('colorPressed', function(colorNum){
   // console.log("Got colorPressed: " + colorNum);
   //disable button --change to grey
-  // $('#' + 'circle' + colorNum).addClass('turnGray');
-  setTimeout(function() { unbinding(colorNum); }, 8000);
-
+  $('#' + 'circle' + colorNum).addClass('turnGray');
+  $('#' + 'circle' + colorNum).unbind("vmousedown", tapholdHandler); 
 
   // set timeout after 8 seconds to release the button 
   // setTimeout(function() { turnGray(colorNum); }, 8000);
   // setTimeout(function() { removeTap(colorNum); }, 8000);
   // console.log('colorNum: ' + colorNum + ' is taken!');  
 });
-
-function unbinding(colorNum){
-  $('#' + 'circle' + colorNum).unbind("vmousedown", tapholdHandler); 
-}
 
 // function turnGray(colorNum){
 //     $('#' + 'circle' + colorNum).addClass('turnGray');
@@ -155,10 +150,6 @@ socket.on('toClients', function(colorNum){
   // $('#' + 'circle' + colorNum).removeClass('turnGray');
   // console.log('colorNum: ' + colorNum + ' is no longer taken');
 });
-
-// function binding(colorNum){
-//   $('#' + 'circle' + colorNum).bind("vmousedown", tapholdHandler); 
-// }
 
 // ********************************************************** 
 // BOOTSTRAP 
