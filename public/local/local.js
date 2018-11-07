28e4d7c7ef0f24d4c6e45d113cebd684b56cc797
rbs517 <rebeccaskurnik@gmail.com>
2018-11-06 21:34:22 -0500

    testing sound

--- public/local/local.js
+++ public/local/local.js
@@ -3,7 +3,7 @@
 
 // Declare Global Variables
 var serial; // variable to hold an instance of the serialport library
-var portName = '/dev/cu.usbmodem14131'; // fill in your serial port name here -- CHANGE ME!
+var portName = '/dev/cu.usbmodemFA131'; // fill in your serial port name here -- CHANGE ME!
 var options = {
   baudrate: 9600 // change the data rate to whatever you wish -- MAKE ME MATCH!
 }; 
