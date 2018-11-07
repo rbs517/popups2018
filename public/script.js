3e5dd756c330d1e2c42e6432227b000d0653d33e
rbs517 <rebeccaskurnik@gmail.com>
2018-11-07 13:23:56 -0500

    changing ports

--- public/script.js
+++ public/script.js
@@ -11,7 +11,7 @@ var sound = [];
 var thisDevice;
 var buttonStatusList = [];
 var myActiveButtons = [false,false,false,false,false,false,false,false,false,false]; 
-var buttonColors = ['#ff0019', '#ed2157', '#ff5900', '#ffe500', '#b0f442', '#9724F3', '#D053F1', '#639EFC', '#7DD7FA', '#B2FFFA'];
+var buttonColors = ['#ff0019', '#ed2157', '#ff5900', '#ffe500', '#b0f442', '#9724F3', '#D053F1', '#639EFC', '#7DD7FA', '#8c00ff'];
 var circleNumber;
 var counter = 0;
 
