87d4e535c32f1e29a2bfbdaf0b3e32dc2b01b451
rbs517 <rebeccaskurnik@gmail.com>
2018-11-08 18:02:29 -0500

    trying to allow access to microphone on chrome

--- public/script.js
+++ public/script.js
@@ -201,7 +201,7 @@ function sendMicData(colorNum) {
   
 
       // Get mic input value 
-      var micMapped = constrain(map(vol, 0, 0.15, 1, 9), 4, 9); // inputVal is for arduino to control the fan
+      var micMapped = constrain(map(vol, 0, 0.07, 1, 9), 4, 9); // inputVal is for arduino to control the fan
       micInput = Math.floor(micMapped);
 
         console.log("gonna send micVal " + micInput + " and colorNum " + colorNum + " to the server");
