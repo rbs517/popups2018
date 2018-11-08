1e5a988b3708dc1da937b25cc6ed991fa594c89d
rbs517 <rebeccaskurnik@gmail.com>
2018-11-08 14:32:36 -0500

    trying to allow access to microphone on chrome

--- public/script.js
+++ public/script.js
@@ -22,7 +22,6 @@ function setup() {
   noCanvas();
   // mic setup
   mic = new p5.AudioIn();
-  hasGetUserMedia();
   mic.start();
   
   // load sounds
@@ -42,16 +41,69 @@ function draw() {
 
 }
 
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
+// Allow Chrome to access microphone 
+
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+/* global AudioContext, SoundMeter */
+
+'use strict';
+
+var instantMeter = document.querySelector('#instant meter');
+var slowMeter = document.querySelector('#slow meter');
+var clipMeter = document.querySelector('#clip meter');
+
+var instantValueDisplay = document.querySelector('#instant .value');
+var slowValueDisplay = document.querySelector('#slow .value');
+var clipValueDisplay = document.querySelector('#clip .value');
+
+try {
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  window.audioContext = new AudioContext();
+} catch (e) {
+  alert('Web Audio API not supported.');
 }
 
-if (hasGetUserMedia()) {
-  console.log('Good to go!');
-} else {
-  alert('getUserMedia() is not supported by your browser');
+// Put variables in global scope to make them available to the browser console.
+var constraints = window.constraints = {
+  audio: true,
+  video: false
+};
+
+function handleSuccess(stream) {
+  // Put variables in global scope to make them available to the
+  // browser console.
+  window.stream = stream;
+  var soundMeter = window.soundMeter = new SoundMeter(window.audioContext);
+  soundMeter.connectToSource(stream, function(e) {
+    if (e) {
+      alert(e);
+      return;
+    }
+    setInterval(() => {
+      instantMeter.value = instantValueDisplay.innerText =
+        soundMeter.instant.toFixed(2);
+      slowMeter.value = slowValueDisplay.innerText =
+        soundMeter.slow.toFixed(2);
+      clipMeter.value = clipValueDisplay.innerText =
+        soundMeter.clip;
+    }, 200);
+  });
 }
+
+function handleError(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+
+
 
 // Circles placed in a circle design for taphold page
 var type = 1, // circle type - 1 whole, 0.5 half, 0.25 quarter
